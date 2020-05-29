52da4711a8df1b53cbce38f985d758154eafa4a0
pei_wang <pwang@ascap.com>
2020-05-29 09:59:53 -0400

    test

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^test/$', views.post_test, name='post_test')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,10 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+def post_test(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
